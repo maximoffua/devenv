8d5a6e3e74e2866ece026d858da931b9b697581f
Sergii Maksymov <ua.maximoff@gmail.com>
2024-02-23 15:57:14 +0100

    add: mutagen.io integration for background file sync and port forwarding
    Mutagen.io provides file synchronization with remote host or docker container and port forwarding.
    Roadmap:
    - ignore .devenv/ and .direnv by default
    - add support for .gitignore file lists
    - get rid of "Error: project already started" message

---
+++ src/modules/integrations/mutagen.nix
@@ -0,0 +1,51 @@
+{ pkgs
+, lib
+, config
+, ...
+}:
+let
+  inherit (lib) types;
+  cfg = config.mutagen;
+  settingsFormat = pkgs.formats.yaml { };
+  file = settingsFormat.generate "mutagen.yaml" cfg.settings;
+  configPath = "${config.env.DEVENV_STATE}/mutagen.yaml";
+in
+{
+  options.mutagen = {
+    enable = lib.mkEnableOption "integration with mutagen.io for file synchronization and port forwarding";
+
+    package = lib.mkOption {
+      type = types.package;
+      default = pkgs.mutagen;
+      description = lib.mdDoc ''
+        Package for Mutagen.io installation.
+      '';
+    };
+
+    settings = lib.mkOption {
+      type = types.submodule {
+        freeformType = settingsFormat.type;
+      };
+    };
+
+    default = { };
+
+    description = lib.mdDoc ''
+      Mutagen settings which are written to mutagen.yaml project configuration.
+    '';
+  };
+
+  config = lib.mkIf config.mutagen.enable {
+    env.MUTAGEN_PROJECT_FILE = configPath;
+    packages = [ cfg.package ];
+
+    enterShell = ''
+      if [ ! -f ${configPath} ]; then
+        mkdir -p "$(dirname "${configPath}")"
+        cat ${file} > ${configPath}
+      fi
+      mutagen daemon start
+      mutagen project -f ${configPath} start
+    '';
+  };
+}
